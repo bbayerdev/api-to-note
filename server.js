9a6530da4fca366c1bb1ffd06399610896662fc2
bayer <bbayerdev@gmail.com>
2025-02-07 10:03:26 -0300

    add demo note in neewUser

--- server.js
+++ server.js
@@ -44,6 +44,150 @@ app.post('/usuario', async (req, res) => {
       },
     })
 
+    await prisma.note.create({
+      data: {
+        title: 'Demo Note',
+        content: [
+          {
+            type: "heading",
+            content: "Welcome to this demo!",
+          },
+          {
+            type: "paragraph",
+          },
+          {
+            type: "paragraph",
+            content: [
+              {
+                type: "text",
+                text: "Blocks:",
+                styles: { bold: true },
+              },
+            ],
+          },
+          {
+            type: "paragraph",
+            content: "Paragraph",
+          },
+          {
+            type: "heading",
+            content: "Heading",
+          },
+          {
+            type: "bulletListItem",
+            content: "Bullet List Item",
+          },
+          {
+            type: "numberedListItem",
+            content: "Numbered List Item",
+          },
+          {
+            type: "checkListItem",
+            content: "Check List Item",
+          },
+          {
+            type: "paragraph",
+          },
+          {
+            type: "codeBlock",
+            props: { language: "javascript" },
+            content: "console.log('Hello, world!');",
+          },
+          {
+            type: "paragraph",
+          },
+          {
+            type: "table",
+            content: {
+              type: "tableContent",
+              rows: [
+                {
+                  cells: ["Table Cell", "Table Cell", "Table Cell"],
+                },
+                {
+                  cells: ["Table Cell", "Table Cell", "Table Cell"],
+                },
+                {
+                  cells: ["Table Cell", "Table Cell", "Table Cell"],
+                },
+              ],
+            },
+          },
+          {
+            type: "file",
+          },
+          {
+            type: "image",
+            props: {
+              url: "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+              caption:
+                "From https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+            },
+          },
+          {
+            type: "video",
+            props: {
+              url: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm",
+              caption:
+                "From https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm",
+            },
+          },
+          {
+            type: "audio",
+            props: {
+              url: "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3",
+              caption:
+                "From https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3",
+            },
+          },
+          {
+            type: "paragraph",
+          },
+          {
+            type: "paragraph",
+            content: [
+              {
+                type: "text",
+                text: "Inline Content:",
+                styles: { bold: true },
+              },
+            ],
+          },
+          {
+            type: "paragraph",
+            content: [
+              {
+                type: "text",
+                text: "Styled Text",
+                styles: {
+                  bold: true,
+                  italic: true,
+                  textColor: "red",
+                  backgroundColor: "blue",
+                },
+              },
+              {
+                type: "text",
+                text: " ",
+                styles: {},
+              },
+              {
+                type: "link",
+                content: "Link",
+                href: "https://www.blocknotejs.org",
+              },
+            ],
+          },
+          {
+            type: "paragraph",
+          }],
+
+        date: new Date().toISOString(),
+        hour: new Date().toLocaleTimeString(),
+        idUser: novoUsuario.id,
+      }
+    })
+
     res.status(201).json(novoUsuario);
   } catch (error) {
     console.error('Erro ao criar usuário:', error)
@@ -161,7 +305,9 @@ app.post('/usuario/:id/notes', async (req, res) => {
         hour,
         idUser: id,
       }
-    });
+    })
+
+
 
     res.status(201).json(novaNota);
   } catch (error) {
